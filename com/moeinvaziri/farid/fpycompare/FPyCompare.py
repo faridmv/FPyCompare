fd4d838a9b7a0f621a88b40ea6a1dccef908daa5
faridmv <faridmv@gmail.com>
2020-04-04 21:46:53 +0430

    The ability of writing the result to files has been added

--- com/moeinvaziri/farid/fpycompare/FPyCompare.py
+++ com/moeinvaziri/farid/fpycompare/FPyCompare.py
@@ -4,33 +4,38 @@ class FPyCompare:
             lines = f.read().splitlines()
         return lines
 
+    def __writeFile(self, resultList, fileName):
+        with open(fileName, "w") as outfile:
+            outfile.write("\n".join(resultList))
+        print(fileName + " completed!")
+
     def initializeList(self):
         fileNameOne = input("First File Name: ")
         self.listOne = set(self.__readFile(fileNameOne))
         fileNameTwo = input("Second File Name: ")
         self.listTwo = set(self.__readFile(fileNameTwo))
 
-    def interset(self):
+    def interset(self, printResult=True):
         intersect = list(self.listOne.intersection(self.listTwo))
-        print(intersect)
+        print(intersect) if printResult else self.__writeFile(intersect, "result_intersect.txt")
 
-    def union(self):
+    def union(self, printResult=True):
         union = list(self.listOne.union(self.listTwo))
-        print(union)
+        print(union) if printResult else self.__writeFile(union, "result_union.txt")
 
-    def differenceListOne(self):
+    def differenceListOne(self, printResult=True):
         subtract = list(self.listOne.difference(self.listTwo))
-        print(subtract)
+        print(subtract) if printResult else self.__writeFile(subtract, "result_DifferenceOne.txt")
 
-    def differenceListTwo(self):
+    def differenceListTwo(self, printResult=True):
         subtract = list(self.listTwo.difference(self.listOne))
-        print(subtract)
+        print(subtract) if printResult else self.__writeFile(subtract, "result_DifferenceTwo.txt")
 
 
 fCompare = FPyCompare()
 
 fCompare.initializeList()
-fCompare.interset()
-fCompare.union()
-fCompare.differenceListOne()
-fCompare.differenceListTwo()
+fCompare.interset(False)
+fCompare.union(False)
+fCompare.differenceListOne(False)
+fCompare.differenceListTwo(False)
